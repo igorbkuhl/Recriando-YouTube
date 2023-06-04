4de6a58516ebade11dad362778d337aaef0d5238
Igor Borges <igorb.kuhl@gmail.com>
2023-06-04 20:53:57 -0300

    iniciando o header da página

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <div id="header">
+      <img src=".\assets\YouTube_Logo_2017.svg.webp" alt="YouTube Logo" />
+    </div>
+  </header>
+  <!-- <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav> -->
+  <router-view />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
